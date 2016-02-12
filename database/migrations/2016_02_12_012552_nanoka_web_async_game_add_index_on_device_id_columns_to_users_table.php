cd6ecf9be475f35b21b2a59b256bbe8df5522eb6
Monnot Stéphane <monnot.stephane@gmail.com>
2016-02-12 02:16:26 +0100

    Add new asyncgame migrations files

---
+++ database/migrations/2016_02_12_012552_nanoka_web_async_game_add_index_on_device_id_columns_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class NanokaWebAsyncGameAddNameColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('async_game_users', function (Blueprint $table) {
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('async_game_users', function (Blueprint $table) {
+            $table->dropColumn(
+                'name'
+            );
+        });
+    }
+}
---
+++ database/migrations/2016_02_12_013052_nanoka_web_async_game_add_name_columns_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class NanokaWebAsyncGameAddIndexOnDeviceIdColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('async_game_users', function (Blueprint $table) {
+            $table->index('device_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('async_game_users', function (Blueprint $table) {
+            $table->dropIndex(
+                'device_id'
+            );
+        });
+    }
+}
